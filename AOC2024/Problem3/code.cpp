547676aa64e29e4164ec17e7ff6481e2796d395e
TejasweeSulekh <20d070082@iitb.ac.in>
2024-12-19 16:19:56 +0530

    Implemented part 1

--- AOC2024/Problem3/code.cpp
+++ AOC2024/Problem3/code.cpp
@@ -33,19 +33,10 @@ bool checkValidity(std::string &s, int ptr, std::pair<int, int> &mulPair){
         ptr++;
         std::string tempNum;
         fetchNumber(s, ptr, tempNum);
-        
-        // for(int i = 0; i < 3; i++){
-        //     if(s[ptr] >= '0' && s[ptr] <= '9'){
-        //         tempNum += s[ptr];
-        //         ptr++;
-        //     }
-        //     else{
-        //         break;
-        //     }
-        // }
         if(!tempNum.empty()){
             tempNum1 = stringToInt(tempNum);
             tempNum.clear();
+            // ptr++;
             if(s[ptr] == ','){
                 ptr++;
                 fetchNumber(s, ptr, tempNum);
@@ -64,11 +55,11 @@ bool checkValidity(std::string &s, int ptr, std::pair<int, int> &mulPair){
     return false;
 }
 
-int multiplier(std::string &s){
+long long multiplier(std::string &s){
     std::vector<char> lookup = {'m', 'u', 'l'};//look for a instance of mull occurance
     int n = s.size();
     int counter = 0;
-    int accumulater = 0;
+    long long accumulater = 0;
     std::pair<int, int> mulPair;
     for(int i = 0; i < n; i++){
         char c = s[i];
@@ -80,7 +71,7 @@ int multiplier(std::string &s){
         }
         if(counter == 3){
             counter = 0;
-            if(checkValidity(s, i, mulPair)){
+            if(checkValidity(s, i+1, mulPair)){
                 accumulater += mulPair.first * mulPair.second;
             }
         }
@@ -91,13 +82,12 @@ int multiplier(std::string &s){
 
 
 int main(){
-    std::ifstream f("testInput.txt");
+    std::ifstream f("input.txt");
     std::string s, input;
+    long long result = 0;
     while(getline(f, s)){
-        input += s;
+        result += multiplier(s);
     }
-    int result = 0;
-    result = multiplier(s);
     std::cout << "from the valid mul instructions the answer is " << result << std::endl;
     return 0;
 }
