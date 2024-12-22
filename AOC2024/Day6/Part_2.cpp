41571c1ce63f64ddfb28f1fda7376d8f5bc4d900
TejasweeSulekh <20d070082@iitb.ac.in>
2024-12-22 12:15:31 +0530

    Implemented Day 6 part 2

--- AOC2024/Day6/Part_2.cpp
+++ AOC2024/Day6/Part_2.cpp
@@ -22,8 +22,10 @@ bool inBound(int row, int col, int rowSize, int colSize){
     }
 }
 
-bool pathTraverser(int row, int col, long &uniqueCounter, int rowSize, int colSize, std::vector<std::pair<int, int>> &Directions, 
-    std::vector<std::vector<bool>> &visited, std::vector<std::vector<char>> &grid, int &direc){
+bool pathTraverser(int row, int col, long uniqueCounter, int rowSize, int colSize, std::vector<std::pair<int, int>> &Directions, 
+    std::vector<std::vector<bool>> &visited, std::vector<std::vector<char>> &grid){
+    int lastNewRow = row, lastNewCol = col;
+    int direc = 0;
     while(inBound(row, col, rowSize, colSize)){
         int newRow = row + Directions[direc].first;
         int newCol = col + Directions[direc].second;
@@ -33,7 +35,12 @@ bool pathTraverser(int row, int col, long &uniqueCounter, int rowSize, int colSi
         if(grid[newRow][newCol] != '#'){ //No obstacle
             row = newRow;
             col = newCol;
+            if((row == lastNewRow) && (col == lastNewCol)){
+                return true;
+            }
             if(!visited[row][col]){
+                lastNewCol = col;
+                lastNewRow = row;
                 visited[row][col] = 1;
                 uniqueCounter++;
             }
@@ -45,6 +52,31 @@ bool pathTraverser(int row, int col, long &uniqueCounter, int rowSize, int colSi
     return true;
 }
 
+void ObstacleCounter(int row, int col, long &uniqueCounter, int rowSize, int colSize, std::vector<std::pair<int, int>> &Directions, 
+    std::vector<std::vector<bool>> &visited, std::vector<std::vector<char>> &grid){
+    long temp = 0;
+    for(int i = 0; i < rowSize; i++){
+        for(int j = 0; j < colSize; j++){
+            std::vector<std::vector<bool>> tempVisited(rowSize, std::vector<bool>(colSize, 0));
+            tempVisited[row][col] = 1;
+            // Check if the guard even goes through that path
+            if(visited[i][j] == 0){
+                continue;
+            }
+            // Test if adding an obstacle there is helpful or not
+            if(grid[i][j] != '#' && !(i == row && j == col)){
+                grid[i][j] = '#'; // Add obstacle
+                // Check if a loop happens
+                if(pathTraverser(row, col, temp, rowSize, colSize, Directions, tempVisited, grid)){
+                    uniqueCounter++;
+                }
+                grid[i][j] = '.'; // Remove obstacle
+            }
+        }
+    }
+    return;
+}
+
 long UniquePathUsedFinder(std::vector<std::vector<char>> &grid, int row, int col){
     int rowSize = grid.size(), colSize = grid[0].size();
     if(rowSize == 0){
@@ -53,11 +85,12 @@ long UniquePathUsedFinder(std::vector<std::vector<char>> &grid, int row, int col
     }
     std::vector<std::vector<bool>> visited(rowSize, std::vector<bool>(colSize, 0)); //Visited array stored bool i.e. visited or not visited
     std::vector<std::pair<int, int>> Directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}}; // Direction which the guard is facing
-    int direc = 0;
     long uniqueCounter = 1;
     visited[row][col] = 1;
     // if part 1
-    pathTraverser(row, col, uniqueCounter, rowSize, colSize, Directions, visited, grid, direc);
+    pathTraverser(row, col, uniqueCounter, rowSize, colSize, Directions, visited, grid);
+    uniqueCounter = 0; // Resetting the counter
+    ObstacleCounter(row, col, uniqueCounter, rowSize, colSize, Directions, visited, grid);
 
     return uniqueCounter;
 }
@@ -65,7 +98,7 @@ long UniquePathUsedFinder(std::vector<std::vector<char>> &grid, int row, int col
 
 
 int main(){
-    std::ifstream f("testInput.txt");
+    std::ifstream f("input.txt");
     std::string s;
     std::vector<std::vector<char>> grid;
     int row = 0, col = 0; // The starting index (0-based indexing)
@@ -83,8 +116,8 @@ int main(){
         }
     }
     // std::cout << row << " " << col << std::endl;
-    long uniqueCounter = UniquePathUsedFinder(grid, row, col);
-    std::cout << "The unique path blocks traversed are " << uniqueCounter << std::endl;
-
+    long uniqueCounter = 0;
+    uniqueCounter = UniquePathUsedFinder(grid, row, col);
+    std::cout << "Possible obstacle placements are " << uniqueCounter << std::endl;
     return 0;
 }
